62498bbef21eacce4e4726a56e2dcaa111024009
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-04-21 21:50:28 -0300

    feat: created Movement class

---
+++ src/Movement.java
@@ -0,0 +1,11 @@
+public class Movement {
+  private String description;
+  private char type;
+  private double value;
+
+  public Movement(String description, char type, double value) {
+    this.description = description;
+    this.type = type;
+    this.value = value;
+  }
+}
